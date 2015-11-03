0c856dd04778498a4e2881d0de2decd2c56d817f
Rjburghardt <rjburghardt@gmail.com>
2015-11-03 14:48:46 -0700

    Add products.js to collections

---
+++ src/collections/products.js
@@ -0,0 +1,22 @@
+var Backbone = require('backbone');
+
+
+/****************************************
+  App
+*****************************************/
+
+var App = require('../app');
+var Product = require('../models/product.js');
+
+/****************************************
+  Collection: User
+*****************************************/
+
+var ProductCollection = Backbone.Collection.extend({
+  url: App.Settings.apiRoot + '/products',
+  model: Product
+});
+
+App.Collections.products = new ProductCollection;
+
+module.exports = App.Collections.products;
