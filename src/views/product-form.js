463bfce30d6e0fd412ba32dedfb16e149af5972d
Rjburghardt <rjburghardt@gmail.com>
2015-11-05 13:54:48 -0700

    Product form edit

--- src/views/product-form.js
+++ src/views/product-form.js
@@ -18,7 +18,7 @@ var ProductFormView = Backbone.View.extend({
   el: $("main"),
   editMode: false,
 
-  render: function (productId, userId) {
+  render: function (userId, productId) {
     var _this = this;
     this.editMode = !!productId;
 
